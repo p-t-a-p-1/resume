dc0b10c058c22ae88ea78b19662eb4be60afdd9d
p-t-a-p-1 <kb240225+1@gmail.com>
2020-09-11 00:04:47 +0900

    [update] ページ間を遷移する際にフェードさせるアニメーションを追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,9 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: [
+    '~/assets/main.css',
+  ],
   /*
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
